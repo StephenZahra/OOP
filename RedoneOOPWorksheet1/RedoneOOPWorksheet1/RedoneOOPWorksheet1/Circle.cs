15e210d09274c4b421dbb03c5322070391505e5c
Stephen Zahra Dedomenico <Stephen.Dedomenico.c10374@mcast.edu.mt>
2020-10-23 11:45:14 +0200

    Did worksheet 1 Question 1 (Used constructor)

---
+++ RedoneOOPWorksheet1/RedoneOOPWorksheet1/RedoneOOPWorksheet1/Circle.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RedoneOOPWorksheet1
+{
+    //access modifier - are the level of access to the class and its members
+    //e.g public/ private/ protected
+    class Circle
+    {
+        //constructor - bears the same name as the class, which is used to create an instance of a class (the object)
+        //      secondary objective of a constructor: it can be used as a normal method
+        public Circle() //the constructor is like the key to start the class / the key to initialize the object
+        {
+            Colour = "Black";
+        }
+
+        public string Colour { get; set; }
+        public double Radius { get; set; } //Property
+
+        /// <summary>
+        /// Displays the radius value of the instance when called
+        /// </summary>
+        /// <returns></returns>
+
+        public string GetRadius()
+        {
+            return "Radius: " + Radius;
+        }
+
+        public double GetArea()
+        {
+            return Math.PI * Radius * Radius;
+        }
+
+        public double GetPerimeter()
+        {
+            return 2 * Math.PI * Radius;
+        }
+
+    }
+}
---
+++ RedoneOOPWorksheet1/RedoneOOPWorksheet1/RedoneOOPWorksheet1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RedoneOOPWorksheet1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //object (instance) --> instance is when you create space in memory and you start assigning values to the template you created earlier
+            Circle c = new Circle(); //e.g of an object --> c
+            c.Radius = 5;
+
+            Console.WriteLine("Input a radius: ");
+            c.Radius = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine($"Area is {Math.Round(c.GetArea(), 2)}");
+            Console.WriteLine($"Perimeter is {Math.Round(c.GetPerimeter(), 2)}");
+
+            Console.WriteLine("Press a key to exit");
+            Console.ReadKey();
+
+
+        }
+    }
+}
