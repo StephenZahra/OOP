09d2650a9e195a198ed405499e5f7fa38ee5ee09
Stephen Zahra Dedomenico <Stephen.Dedomenico.c10374@mcast.edu.mt>
2020-10-29 15:27:12 +0100

    Worksheet 3

---
+++ Worksheet3/Worksheet3/Magazine.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Worksheet3
+{
+    class Magazine : Publication
+    {
+        public int CurrIssue { get; set; }
+        public int OrderQty { get; set; }
+
+        public void AdjustQty(int amount)
+        {
+            OrderQty = amount;
+        }
+
+        public void RecNewIssue()
+        {
+            CurrIssue++;
+        }
+    }
+}
