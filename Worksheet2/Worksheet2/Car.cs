1cc12eac0881349a3affa673c3de0bcc7e32da0c
Stephen Zahra Dedomenico <Stephen.Dedomenico.c10374@mcast.edu.mt>
2020-10-30 18:52:04 +0100

    Finished first half, strating while loop menu functionality

---
+++ Worksheet2/Worksheet2/Car.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Worksheet2
+{
+    public class Car
+    {
+        public Car(string manufacturer, string colour)
+        {
+            Manufacturer = manufacturer;
+            Colour = colour;
+        }
+
+        public string Manufacturer { get; set; }
+        public string Colour { get; set; }
+        public string OdometerReading { get; set; }
+        public string TypeOfFuel { get; set; }
+        public double Litres { get; set; }
+
+        public string Drive(string destination)
+        {
+            if(destination == null)
+            {
+                return ("Please input a destination");
+            }
+            else
+            {
+                return ("Correct destination inputted, commencing journey");
+            }
+        }
+
+        public void FillTank(int monetaryAmount)
+        {
+            if(Manufacturer == null || Colour == null || OdometerReading == null || TypeOfFuel == null || Litres < 0)
+            {
+                Console.WriteLine("Please enter car details before refueling");
+            }
+            else
+            {
+                if (TypeOfFuel == "Diesel")
+                {
+                    Console.WriteLine("Insert the amount of money you wish to input: ");
+                    monetaryAmount = Convert.ToInt32(Console.ReadLine());
+                    Litres += monetaryAmount * 0.71;
+                    Console.WriteLine(Litres.ToString());
+                }
+                else if(TypeOfFuel == "petrol")
+                {
+                    Console.WriteLine("Insert the amount of money you wish to input: ");
+                    monetaryAmount = Convert.ToInt32(Console.ReadLine());
+                    Litres += monetaryAmount * 0.65;
+                    Console.WriteLine(Litres.ToString());
+                }
+            }
+        }
+
+        public string CarDetails(string fuel, string odometerReading, string colour)
+        {
+            if(fuel != "petrol" || fuel != "Petrol" || fuel != "diesel" || fuel != "Diesel")
+            {
+                return "The inputted type of fuel is incorrect";
+            }
+            else
+            {
+                return string.Empty;
+            }
+        }
+
+        public void RePaint(string colour)
+        {
+            Colour = colour;
+        }
+    }
+}
---
+++ Worksheet2/Worksheet2/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Worksheet2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car c1 = new Car("Honda", "White");
+            Car c2 = new Car("Fiat", "Blue");
+
+            List<string> Cars = new List<string>();
+            Cars.Add(c1.Manufacturer);
+            Cars.Add(c2.Manufacturer);
+
+            Console.WriteLine("Choose the car which you are going to use today:");
+            Console.WriteLine($"1. {Cars[0]} 2. {Cars[1]}");
+            Console.WriteLine("Choose: ");
+
+            string userChoice = null;
+            while (userChoice != "5")
+            {
+                Console.WriteLine("Car Menu\n======");
+                Console.WriteLine("1. Drive Car");
+                Console.WriteLine("2. Fill with fuel");
+                Console.WriteLine("3. Change car details");
+                Console.WriteLine("4. Re-paint");
+                Console.WriteLine("5. Exit");
+                Console.WriteLine("Choose Option: ");
+
+                userChoice = Console.ReadLine();
+
+                if(userChoice == "1")
+                {
+                    Console.WriteLine("Please input your destination: ");
+                    string destination = Console.ReadLine();
+                    c1.Drive(destination);
+                }
+                else if(userChoice == "2")
+                {
+
+                }
+                else if (userChoice == "3")
+                {
+
+                }
+                else if (userChoice == "4")
+                {
+
+                }
+                else if (userChoice == "5")
+                {
+                    Console.WriteLine("Now exiting..press any key to exit");
+                    Console.ReadKey();
+                }
+                else
+                {
+                    Console.WriteLine("Invalid Menu Option!");
+                }
+            }
+        }
+    }
+}
